e07aead273a1ce98f4c34cca38d751e3935d839c
Eric O'Brien <obrienjeric@gmail.com>
2021-09-24 13:08:23 -0400

    extra credit completed

--- sql-skills.sql
+++ sql-skills.sql
@@ -61,3 +61,54 @@ SELECT al.title, ar.name
 FROM album al
 JOIN artist ar ON al.artist_id = ar.artist_id
 ORDER BY ar.name;
+
+-- extra credit
+
+
+--artist
+
+SELECT * FROM artist ORDER BY name desc limit 10;
+
+SELECT * FROM artist WHERE name LIKE 'Black%';
+
+SELECT * FROM artist WHERE name LIKE '%Black%';
+
+--employee
+
+SELECT max(birth_date) FROM employee;
+
+SELECT min(birth_date) FROM employee;
+
+
+--invoice
+
+SELECT COUNT (invoice_id)
+FROM invoice
+WHERE billing_state IN ('CA', 'TX', 'AZ');
+
+SELECT AVG(total)
+FROM invoice;
+
+--more join queries
+
+SELECT pt.track_id
+FROM playlist_track pt
+JOIN playlist p ON pt.playlist_id = p.playlist_id
+where p.name = 'Music';
+
+SELECT t.name
+FROM track t
+JOIN playlist_track p ON t.track_id = p.track_id
+WHERE p.playlist_id = 5;
+
+SELECT t.name, pl.name
+FROM track t
+JOIN playlist_track pt ON t.track_id = pt.track_id
+JOIN playlist pl ON pl.playlist_id = pt.playlist_id;
+
+SELECT t.name, al.title
+FROM track t
+JOIN genre g ON t.genre_id = g.genre_id
+JOIN album al ON t.album_id = al.album_id
+WHERE g.name = 'Alternative' or g.name = 'Punk';
+
